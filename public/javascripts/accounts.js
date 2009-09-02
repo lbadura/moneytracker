c8f83bfad5f9f75813454d5dfd5fbb186a850560
Łukasz Badura <lukasz@niebo.net>
2009-09-02 23:47:52 +0200

    Fixed account management

--- public/javascripts/accounts.js
+++ public/javascripts/accounts.js
@@ -4,76 +4,71 @@ mt.accounts = {};
  *  New account form
  */
 mt.accounts.newAccountForm = function() {
-    this.form = $("#new-account-form");
-    $.ajax({
-        data: {
-            'authenticity_token' : authenticityToken
-        },
-        url: new_account_path(""),
-        type: 'GET',
-        dataType: 'html',
-        success: function(data) {
-            $.facebox(data);
-            $('#add-account-button').bind('click', function(event) {
-                event.stopPropagation();
-                event.preventDefault();
-                $.ajax({
-                    data: $(this.form).serialize(),
-                    url: account_path("create"),
-                    type: 'POST',
-                    dataType: 'json',
-                    success: function(envelope) {
-                        if (envelope.ok) {
-                            $("div#account-list").load(account_path('refresh'));
-                            $.facebox.close();
-                        } else {
-                            if (envelope.errors) {
-                                mt.forms.displayRecordErrors('account', envelope);
-                            }
-                        }
-                    }
-                });
-            });
-        }
-    });
-    return false;
+  this.form = $("#new-account-form");
+  $.ajax({
+    url: new_account_path(""),
+    type: 'GET',
+    dataType: 'html',
+    success: function(data) {
+      $.facebox(data);
+      $('#add-account-button').bind('click', function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        $.ajax({
+          data: $(this.form).serialize(),
+          url: accounts_path(''),
+          type: 'POST',
+          dataType: 'json',
+          success: function(envelope) {
+            if (envelope.ok) {
+              $(document).trigger("mt:accountsChanged");
+            } else {
+              if (envelope.errors) {
+                mt.forms.displayRecordErrors('account', envelope);
+              }
+            }
+          }
+        });
+      });
+    }
+  });
+  return false;
 }
 
 /**
  * Edit account button
  */
 mt.accounts.editAccount = function(ev) {
-    accountId = mt.accounts.getAccountId($(ev.target));
-    this.form = $("#new-account-form");
-    $.ajax({
-        url: account_path(accountId) + '/edit',
-        type: 'GET',
-        dataType: 'html',
-        success: function(data) {
-            $.facebox(data);
-            $('#add-account-button').bind('click', function(event) {
-                event.stopPropagation();
-                event.preventDefault();
-                $.ajax({
-                    data: $(this.form).serialize(),
-                    url: account_path("update"),
-                    type: 'POST',
-                    dataType: 'json',
-                    success: function(envelope) {
-                        if (envelope.ok) {
-                            $("div#account-list").load(account_path('refresh'));
-                            $.facebox.close();
-                        } else {
-                            if (envelope.errors) {
-                                mt.forms.displayRecordErrors('account', envelope);
-                            }
-                        }
-                    }
-                });
-            });
-        }
-    });
-    return false;
+  var accountId = mt.accounts.getAccountId($(ev.target));
+  this.form = $("#new-account-form");
+  $.ajax({
+    url: account_path(accountId) + '/edit',
+    type: 'GET',
+    dataType: 'html',
+    success: function(data) {
+      $.facebox(data);
+      $('#add-account-button').bind('click', function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        $.ajax({
+          data: $(this.form).serialize(),
+          url: account_path(accountId),
+          type: 'PUT',
+          dataType: 'json',
+          success: function(envelope) {
+            if (envelope.ok) {
+              $(document).trigger("mt:accountsChanged");
+            } else {
+              if (envelope.errors) {
+                mt.forms.displayRecordErrors('account', envelope);
+              }
+            }
+          }
+        });
+      });
+    }
+  });
+  return false;
 
 }
 
@@ -81,43 +76,46 @@ mt.accounts.editAccount = function(ev) {
  * Handle deleting accounts
  */
 mt.accounts.deleteAccount = function(ev) {
-    accountId = mt.accounts.getAccountId($(ev.target));
-    $.ajax({
-        data: {
-            'authenticity_token' : authenticityToken,
-            'id' : accountId
-        },
-        url: account_path(accountId),
-        type: 'DELETE',
-        dataType: 'json',
-        beforeSend: function() {
-            return confirm("Are you sure you want to delete this account ?")
-            },
-        success: function(envelope) {
-            if (envelope.ok) {
-                var accountsContainer = $("div#account-list");
-                if (accountsContainer.length > 0) {
-                    accountsContainer.load(account_path('refresh'));
-                }
-            } else {
-            // FIXME: handle errors
-            }
-        }
-    });
+  var accountId = mt.accounts.getAccountId($(ev.target));
+  $.ajax({
+    data: {
+      'authenticity_token' : authenticityToken,
+      'id' : accountId
+    },
+    url: account_path(accountId),
+    type: 'DELETE',
+    dataType: 'json',
+    beforeSend: function() {
+      return confirm("Are you sure you want to delete this account ?")
+    },
+    success: function(envelope) {
+      if (envelope.ok) {
+        $(document).trigger("mt:accountsChanged");
+      } else {
+    // FIXME: handle errors
+    }
+    }
+  });
 }
 
 /**
  * Helper function to get account id according to a control icon clicked
  */
 mt.accounts.getAccountId = function(element) {
-    return element.parents().find("div.account-controls").attr("id").substr(17);
+  return element.parents().find("div.account-controls").attr("id").substr(17);
+}
+
+mt.accounts.changedHandler = function() {
+  $("div#account-list").load(accounts_path('/refresh'));
+  $.facebox.close();
 }
 
 /**
  *  Setup all elements
  */
 $(document).ready(function() {
-    $("a#new-account-link").live('click', mt.accounts.newAccountForm);
-    $("a.edit-account-link").live('click', mt.accounts.editAccount);
-    $("a.delete-account-link").live('click', mt.accounts.deleteAccount);
+  $(document).bind("mt:accountsChanged", mt.accounts.changedHandler);
+  $("a#new-account-link").live('click', mt.accounts.newAccountForm);
+  $("a.edit-account-link").live('click', mt.accounts.editAccount);
+  $("a.delete-account-link").live('click', mt.accounts.deleteAccount);
 });
