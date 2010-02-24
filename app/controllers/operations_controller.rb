dd87ee5ad3fdb36f2f5af99d487bd1e688c3ee16
Łukasz Badura <lukasz@niebo.net>
2010-02-24 21:55:32 +0100

    Updated rails. Added mysql gem to vendor.

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -1,6 +1,13 @@
 class OperationsController < ApplicationController
   def index
-    @operations = current_user.operations
+    respond_to do |format|
+      format.html do
+        @operations = current_user.operations
+      end
+      format.json do
+        return render :json => current_user.operations.to_json
+      end
+    end
   end
   
   def new
--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -30,6 +30,7 @@ Rails::Initializer.run do |config|
   config.i18n.load_path = Dir[File.join(RAILS_ROOT, 'locales', '*.{yml}')]
   config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
   config.gem "thoughtbot-shoulda", :lib => 'shoulda/rails', :source => "http://gems.github.com"
+  config.gem "mysql"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
