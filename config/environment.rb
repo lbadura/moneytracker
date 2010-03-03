014ef5d0a9998f59aef156516bb3634d1bd5ef39
Łukasz Badura <lukasz@niebo.net>
2010-03-03 08:06:25 +0100

    Added json gem as a dependecy.

--- config/environment.rb
+++ config/environment.rb
@@ -31,6 +31,7 @@ Rails::Initializer.run do |config|
   config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
   config.gem "thoughtbot-shoulda", :lib => 'shoulda/rails', :source => "http://gems.github.com"
   config.gem "mysql"
+  config.gem "json"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
